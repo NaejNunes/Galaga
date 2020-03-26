b30e0610b13f68f070d721c5b10b6706bd1d75aa
Jean Nunes <jeannobreganunes@hotmail.com>
2020-03-26 02:54:34 -0300

    Work animations with functions in enimy

---
+++ ProjetoGalaga/Assets/Scripts/AlienShotMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienShotMove : MonoBehaviour
+{
+    [SerializeField] protected GameObject projectilePrefab;
+    [SerializeField] protected float projectileSpeed;
+
+    protected GameObject projectileTemp;
+
+    public void missileShoot()
+    {
+        projectileTemp = Instantiate(projectilePrefab);
+        projectileTemp.transform.position = transform.position;
+        projectileTemp.GetComponent<ProjectaleBehaviour>().Shoot(Vector2.down, projectileSpeed);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ProjetoGalaga/Assets/Scripts/AlienSimpleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSimpleBehaviour : MonoBehaviour
+{
+    [SerializeField] protected Animator animatorComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("FlyMove", Random.Range(5, 10));
+    }
+
+    protected void FlyMove()
+    {
+        animatorComponent.SetTrigger("attack");
+
+        Invoke("FlyMove", Random.Range(5, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
